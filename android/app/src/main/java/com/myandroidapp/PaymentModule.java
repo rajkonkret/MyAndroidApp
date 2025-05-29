3a41dad4ac9ffaec83d17bae0956b77d447ddac9
rajkonkret <rajkonkret660@gmail.com>
2025-05-29 16:07:11 +0200

    fix10

--- android/app/src/main/java/com/myandroidapp/PaymentModule.java
+++ android/app/src/main/java/com/myandroidapp/PaymentModule.java
@@ -1,24 +1,31 @@
-public package pcom.myandroidapp;
+package com.myandroidapp;
 
 import android.app.Activity;
 import android.content.Intent;
 import android.util.Log;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 
+import com.facebook.react.bridge.ActivityEventListener;
+import com.facebook.react.bridge.BaseActivityEventListener;
+import com.facebook.react.bridge.Promise;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
-import com.facebook.react.bridge.Promise;
 
-import java.util.Collections;
-import com.google.gson.Gson;
+import java.util.UUID;
 
 public class PaymentModule extends ReactContextBaseJavaModule {
-    private static final String TAG = "PaymentModule";
 
-    public PaymentModule(@NonNull ReactApplicationContext reactContext) {
+    private static final int PAYMENT_REQUEST_CODE = 1001;
+    private Promise paymentPromise;
+
+    public PaymentModule(ReactApplicationContext reactContext) {
         super(reactContext);
+
+        // Rejestrujemy listener, który będzie odbierał wyniki
+        reactContext.addActivityEventListener(activityEventListener);
     }
 
     @NonNull
@@ -28,34 +35,44 @@ public class PaymentModule extends ReactContextBaseJavaModule {
     }
 
     @ReactMethod
-    public void makePayment(int operatorId, int amount, String phoneNumber, Promise promise) {
+    public void makePayment(String json, Promise promise) {
+        Activity activity = getCurrentActivity();
+
+        if (activity == null) {
+            promise.reject("NO_ACTIVITY", "Activity is null");
+            return;
+        }
+
+        Intent intent = new Intent("com.worldline.payment.action.PROCESS_TRANSACTION");
+        intent.putExtra("WPI_SERVICE_TYPE", "WPI_SVC_PAYMENT");
+        intent.putExtra("WPI_REQUEST", json);
+        intent.putExtra("WPI_VERSION", "2.2ko");
+        intent.putExtra("WPI_SESSION_ID", UUID.randomUUID().toString());
+
+        this.paymentPromise = promise;
+
         try {
-            WpiSaleTransactionRequest request = new WpiSaleTransactionRequest();
-            request.setRequestedAmount(amount);
-            request.setCurrency("PLN");
-            request.setReference(operatorId + "_" + phoneNumber + "_" + amount);
-            request.setReceiptFormat(Collections.singletonList("JSON"));
-
-            String json = new Gson().toJson(request);
-            Intent intent = new Intent("com.worldline.payment.action.PROCESS_TRANSACTION");
-            intent.addFlags(Intent.FLAG_ACTIVITY_REORDER_TO_FRONT);
-            intent.putExtra("WPI_SERVICE_TYPE", "WPI_SVC_PAYMENT");
-            intent.putExtra("WPI_REQUEST", json);
-            intent.putExtra("WPI_VERSION", "2.2");
-            intent.putExtra("WPI_SESSION_ID", UUID.randomUUID().toString()) // ← tu wstaw getSessionId()
-
-            Activity activity = getCurrentActivity();
-            if (activity != null) {
-                activity.startActivity(intent);
-                promise.resolve("OK");
-            } else {
-                promise.reject("NO_ACTIVITY", "Activity is null");
-            }
+            activity.startActivityForResult(intent, PAYMENT_REQUEST_CODE);
         } catch (Exception e) {
-            Log.e(TAG, "makePayment error", e);
-            promise.reject("ERROR", e);
+            this.paymentPromise = null;
+            promise.reject("INTENT_ERROR", e.getMessage(), e);
         }
     }
-} {
-    
+
+    // Listener do odbierania wyników
+    private final ActivityEventListener activityEventListener = new BaseActivityEventListener() {
+        @Override
+        public void onActivityResult(Activity activity, int requestCode, int resultCode, @Nullable Intent data) {
+            if (requestCode == PAYMENT_REQUEST_CODE && paymentPromise != null) {
+                if (resultCode == Activity.RESULT_OK && data != null) {
+                    String response = data.getStringExtra("WPI_RESPONSE");
+                    paymentPromise.resolve(response);
+                } else {
+                    paymentPromise.reject("PAYMENT_FAILED", "Transaction was cancelled or failed");
+                }
+
+                paymentPromise = null;
+            }
+        }
+    };
 }
